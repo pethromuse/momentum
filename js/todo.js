531bcc8681b8a7abe7d38f0eeff38adcf1f7318c
pethromuse <pethromuse@gmail.com>
2022-04-21 00:38:46 +0900

    todo

--- js/todo.js
+++ js/todo.js
@@ -1,21 +1,67 @@
 const todo = document.querySelector('#todo-form input');
-const todoList = document.querySelector('#todo-list');
 const todoForm = document.querySelector('#todo-form');
-todoForm.addEventListener('submit', saveTodoLs);
-//document.addEventListener('submit', saveTodoLs);
+todoForm.addEventListener('submit', handleTodoSubmit);
+const todoList = document.querySelector('#todo-list');
 
-function saveTodoLs(el){
-    el.preventDefault();
-    const key = localStorage.length;
-    localStorage.setItem(`todo${key}`, todo.value);
+const todoArr = [];
+
+function handleTodoSubmit(event){
+    event.preventDefault();    
+    paintTodo(todo.value);
+    todoArr.push(todo.value);
+    saveTodos();
     todo.value = '';
 }
 
-function paintTodoLs(){
-    
+function saveTodos(){
+    localStorage.setItem("todos",JSON.stringify(todoArr));
 }
-let todoItem = '';
+function loadTodos(){
+    const tmpTodos = localStorage.getItem("todos");
+    if(tmpTodos){
+        const parsedTodos = JSON.parse(tmpTodos);
+        parsedTodos.forEach(function(item){
+
+        });
+    }
+}
+function paintTodo(newTodo){
+    const li = document.createElement('li');
+    li.innerHTML = newTodo;
+    const button = document.createElement('button');
+    button.innerHTML = 'X';
+    button.addEventListener('click', removeTodo);
+    li.append(button);
+    todoList.appendChild(li);
+}
+
+function removeTodo(event){
+    event.target.parentElement.remove();
+}
+/*
+function paintTodoLs(){
+    clearTodoLs();
+    todoArr.forEach(function(el,index){
+        let li = document.createElement('li');
+        li.innerHTML = el;
+        let button = document.createElement('button');
+        button.innerHTML = 'X';
+        button.value = index;  
+        button.addEventListener('click', removeTodo);
+        li.append(button);
+        todoList.appendChild(li);
+    });
+}
+
+function clearTodoLs(){
+    while(todoList.children.length > 0){
+        todoList.children[0].remove();
+    }
+}*/
+
+/*let todoItem = '';
 let li = '';
+
 if(localStorage.length > 0){
     for(let i = 0;i <localStorage.length ;i++){
         todoItem = localStorage.getItem(`todo${i}`);
@@ -25,4 +71,4 @@ if(localStorage.length > 0){
             todoList.appendChild(li);
         }
     }
-}
+}*/
